00650751c8ea91ebbd590ee65c5fb94e76c60837
AtanasNaydenov <AtanasNaydenov@outlook.com>
2016-05-21 01:41:41 +0200

    login with linkedin

---
+++ Atanas/config.php
@@ -0,0 +1,7 @@
+<?php
+$baseURL = 'http://localhost/dashboard/';
+$callbackURL = 'http://localhost/dashboard/process.php';
+$linkedinApiKey = '77q2xur72wgqob';
+$linkedinApiSecret = 'jXY110NtZv09gKRw';
+$linkedinScope = 'r_basicprofile r_emailaddress';
+?>
