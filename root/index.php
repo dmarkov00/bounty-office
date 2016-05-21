37cb980ab2c61dc01f9fe070768bd620d1ec4321
Dmitry <dmitry-orlov@hotmail.com>
2016-05-21 18:02:55 +0200

    some stuff

--- root/index.php
+++ root/index.php
@@ -8,37 +8,16 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="default/css/header.css">
+    <link rel="stylesheet" type="text/css" href="default/css/mainbody.css">
     <title>Bounty Office</title>
 </head>
 
 <body>
-<nav class="navbar navbar-inverse">
-  <div class="header container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span> 
-      </button>
-      <a class="navbar-brand" href="#">Bounty office</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li><a href="#">Job Offers</a></li>
-        <li><a href="#"></a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<?php require 'default/pageparts/header.php'; 
+    header('Location: dashboard/');
+    ?>
 
 
-    <div id="footer" class="container-fluid">
-        
-    </div>
 
 </body>
 
--- root/joffercreate/index.php
+++ root/joffercreate/index.php
@@ -1,8 +1,33 @@
-<?php
+<html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../default/css/header.css">
+    <link rel="stylesheet" type="text/css" href="../default/css/mainbody.css">
+    <title>Bounty Office</title>
+</head>
 
+<body>
+<?php require '../default/pageparts/header.php'; 
+    ?>
+    
+    <div id="position_container" class="container-fluid">
+        <div class="col-md-6 col-md-offset-3">
+            <>
+        </div>
+    </div>
+    <div>
+        
+        
+    </div>
+
+
+
+</body>
+
+</html>
