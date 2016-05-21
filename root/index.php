0473b3516b4c3e48745a65e84ee1e3da5d571731
Dmitry <dmitry-orlov@hotmail.com>
2016-05-21 15:36:14 +0200

    new directory for the website's folders

---
+++ root/index.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <title>Personal Page</title>
+</head>
+
+<body>
+    <div id="header" class="scrolled">
+        
+    </div>
+
+
+    <div id="footer" class="container-fluid">
+        
+    </div>
+
+</body>
+
+</html>
+
